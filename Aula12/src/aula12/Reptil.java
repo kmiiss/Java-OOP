3786eab8ea682a233948838f1fdfc80ef824b2b9
Camila Moraes <57414299+kmiiss@users.noreply.github.com>
2020-03-22 00:27:01 -0300

    Runtime polymorphism:
    Lesson 12 in OOP Java; Method overriding.

---
+++ Aula12/src/aula12/Reptil.java
@@ -0,0 +1,20 @@
+package aula12;
+public class Reptil extends Animal {
+    private String corEscama;
+
+    @Override
+    public void locomover() {
+        System.out.println("Rastejando!");
+    }
+
+    @Override
+    public void alimentar() {
+        System.out.println("Comendo Vegetais.");
+    }
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Som de Réptil.");
+        System.out.println("----------------------------------");
+    }
+}
